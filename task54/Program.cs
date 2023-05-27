e925f6942e1bb86aedd279da9893cdd8608cf6f7
evgenyi <fonya93@yandex.ru>
2023-05-27 18:24:24 +0700

    Сортировка строк по убыванию

---
+++ task54/Program.cs
@@ -0,0 +1,83 @@
+﻿// Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+int m = GetPositivUserNumber("Введите количество строк: ", "Ошибка ввода");
+int n = GetPositivUserNumber("Введите количество столбцов: ", "Ошибка ввода");
+int minValue = GetUserNumber("Введите минимальное значение массива: ", "Ошибка ввода");
+int maxValue = GetUserNumber("Введите максимальное значение массива: ", "Ошибка ввода");
+
+int[,] array = GetArray(m, n, minValue, maxValue);
+
+PrintArray(array);
+Console.WriteLine();
+
+int[,] result = GetSort(array);
+
+PrintArray(result);
+
+int[,] GetSort(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1) - 1; k++)
+            {
+                if (arr[i, k] < arr[i, k + 1])
+                {
+                    int temp = arr[i, k];
+                    arr[i, k] = arr[i, k + 1];
+                    arr[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+    return arr;
+}
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int GetPositivUserNumber(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber) && userNumber >= 0)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetUserNumber(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
